60c1d0d68e5ca633d57be7d16873d4ae555b622a
ertkonsert <martaaagren@gmail.com>
2020-09-01 10:51:41 +0200

    h

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Genomgångar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Lektion 1 typ eller nåt
+
+            // File.WriteAllText("words.txt", "hey hopp här finns det text");
+
+            /*string s = File.ReadAllText("words.txt");
+
+            Console.WriteLine(s);
+            Console.ReadLine();    */
+
+            // string[] words = {"hey", "ho", "hipp", "hopp"};
+            // File.WriteAllLines("words.txt", words);
+
+            if(File.Exists("words.txt"))
+            {
+            string[] words = File.ReadAllLines("words.txt");
+            System.Console.WriteLine(words[0]);
+
+            }
+
+
+            
+            
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
